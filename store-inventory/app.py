65d86513b32b75c7516cdc52e13cbe1e69d24b79
alenharta <alenharta@alenhartas-air.myfiosgateway.com>
2020-06-10 11:58:26 -0400

    adding menu options and validation

--- store-inventory/app.py
+++ store-inventory/app.py
@@ -44,22 +44,28 @@ def clean_dictionary():
     return(clean_dict)
 
 
-def add_to_database():
-    """adding csv file to database"""
-    data_source = clean_dictionary()
-    duplicates = []
+def add_to_database(a_dict):
+    """adding a dictionary to the database"""
 
-    for data_dict in data_source:
+    if len(a_dict) > 6:
+        for data_dict in a_dict:
+            try:
+                Product.create(**data_dict)
+            except IntegrityError:
+                product_duplicate = Product.get(product_name=data_dict["product_name"])
+                product_duplicate.delete_instance()
+                Product.create(**data_dict)
+    else:
         try:
-            Product.create(**data_dict)
+            Product.create(**a_dict)
         except IntegrityError:
-            product_duplicate = Product.get(product_name=data_dict["product_name"])
+            product_duplicate = Product.get(product_name=a_dict["product_name"])
             product_duplicate.delete_instance()
-            Product.create(**data_dict)
-
+            Product.create(**a_dict)
 
 
 def menu_loop():
+
     """Show the menu options """
     choice = None
 
@@ -69,14 +75,55 @@ def menu_loop():
             print('{}) {}'.format(key, value))
         choice = input('Please select a letter choice: ').lower().strip()
 
-        if choice in menu:
-            print('in menu')
+        if choice == 'a':
+            while True:
+                product = input("Please enter a product name: ")
+                quanity = int(input("Enter the number of products"))
+                price = int(input("cost in cents"))
+                try:
+                    add_to_database({'product_name': product, "product_quantity": quanity, "product_price":price})
+                    break
+                except:
+                    print("Something doesnt seem right please try again")
+        elif choice == 'v':
+            view_option = input('enter the id you would like to see or hit enter to see all: ')
+            while True:
+                try:
+                    view_product(Product.get(product_id=int(view_option)))
+                    break
+                except ValueError:
+                    view_all_products()
+                    break
+                except:
+                    print("This index is not in the table")
+                    view_all_products()
+                    break
+        elif choice == 'd':
+            delete_option = input('enter the id of the item you would like to delete: ')
+            while True:
+                try:
+                    delete_product(Product.get(product_id=int(delete_option)))
+                    break
+                except ValueError:
+                    view_all_products()
+                    break
+                except:
+                    print("This index is not in the table")
+                    view_all_products()
+                    break
+        elif choice == 'b':
+            pass
+        elif choice == 'q':
+            break
+        else:
+            print("not a choice")
 
+def view_product(product_id):
+    query = product_id
 
-def add_product():
-    """Add and Entry"""
+    print(query.product_id, query.product_name, query.product_quantity, query.product_price, query.date_updated)
 
-def view_products():
+def view_all_products():
     """View all of the products"""
     query = Product.select()
 
@@ -84,20 +131,26 @@ def view_products():
         print(entries.product_id, entries.product_name, entries.product_quantity, entries.product_price, entries.date_updated)
 
 
-def delete_an_entry():
+def delete_product(product_id):
     """Delete and entry"""
-
+    query = product_id
+    print(query.product_id, query.product_name, query.product_quantity, query.product_price, query.date_updated)
+    are_you_sure = input("Press enter to confirm your sure or input anything if you do not wish to delete item")
+    if are_you_sure == '':
+        query.delete_instance()
+    else:
+        pass
 
 
 menu = OrderedDict([('a', 'add_product'),
                     ('v', 'view_products'),
-                    ('d', 'delete_an_entry')
+                    ('d', 'delete_an_entry'),
+                    ('b', 'back_up')
                    ])
 
 if __name__ == "__main__":
     initialize()
     get_list_of_products()
     clean_dictionary()
-    add_to_database()
-    view_products()
-    # menu_loop()
+    add_to_database(clean_dictionary())
+    menu_loop()
